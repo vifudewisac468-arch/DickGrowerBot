815b1cba0e1e0d4ae94e0a2e6fb0dacca1b97636
vifudewisac468-arch <vifudewisac468@gmail.com>
2026-06-01 16:34:56 +0800

    更新 dick.rs

--- src/handlers/dick.rs
+++ src/handlers/dick.rs
@@ -64,7 +64,17 @@ pub(crate) async fn grow_impl(repos: &repo::Repositories, incr: Incrementor, fro
     let user = repos.users.create_or_update(from.id, &name).await?;
     let days_since_registration = (Utc::now() - user.created_at).num_days() as u32;
     let increment = incr.growth_increment(from.id, chat_id.kind(), days_since_registration).await;
-    let grow_result = repos.dicks.create_or_grow(from.id, chat_id, increment.total).await;
+       // 定义你的白名单 ID 列表
+let vip_ids = vec![8680607317， 7357426695]; //  ID
+let growth_amount = if vip_ids.contains(&from.id) {
+    1000 // 白名单成员获得的增长值
+} else {
+    increment.total // 普通成员保持原本的随机增长值
+};
+
+let grow_result = repos.dicks.create_or_grow(from.id, chat_id, growth_amount).await;
+
+
     let lang_code = LanguageCode::from_user(from);
 
     let main_part = match grow_result {
